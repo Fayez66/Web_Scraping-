ae44f3700fbbba517abf9da48fdfa74075c06a74
Abdulrahman-MA <Abdulrahman_m_a@outlook.com>
2025-05-02 06:54:14 +0300

    freelancer expanded

---
+++ freelancer/expanding_data.py
@@ -0,0 +1,18 @@
+import re
+import pandas as pd
+import datetime
+from dateutil.relativedelta import relativedelta
+
+df = pd.read_csv("Cleaned.csv")
+
+# Split Skills only if not null
+df['Skills'] = df['Skills'].apply(lambda x: x.split(',') if isinstance(x, str) else x)
+
+# Strip whitespace if it's a list
+df['Skills'] = df['Skills'].apply(lambda x: [tag.strip() for tag in x] if isinstance(x, list) else x)
+
+# Explode into multiple rows
+df = df.explode('Skills').reset_index(drop=True)
+
+# Save to new file
+df.to_csv("jobs_expanded.csv", index=False, encoding="utf-8-sig")
